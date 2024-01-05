2ade5799b25faa58437c626637165673c5fb529f
Mac_VM <miguelescobar704@gmail.com>
2024-01-05 13:39:36 +0000

    'raycasting' finit mais totalement mdrr

--- srcs/mlx/raycasting/ft_raycasting_calc.c
+++ srcs/mlx/raycasting/ft_raycasting_calc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_raycasting_calc.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: qbanet <qbanet@student.42perpignan.fr>     +#+  +:+       +#+        */
+/*   By: mescobar <mescobar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 10:40:23 by mescobar          #+#    #+#             */
-/*   Updated: 2024/01/05 10:22:22 by qbanet           ###   ########.fr       */
+/*   Updated: 2024/01/05 13:34:06 by mescobar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,6 +41,36 @@ void	ft_step_and_sidedist(t_ray *r)
 	algorithm used to draw lines.
 */
 
+int	ft_not_wall(t_data *l, t_ray *r)
+{
+	if (l->map[(int)r->mapy][(int)r->mapx] != '0'
+		&& l->map[(int)r->mapy][(int)r->mapx] != 'N'
+		&& l->map[(int)r->mapy][(int)r->mapx] != 'S'
+		&& l->map[(int)r->mapy][(int)r->mapx] != 'E'
+		&& l->map[(int)r->mapy][(int)r->mapx] != 'O')
+		return (1);
+	return (0);
+}
+
+int	ft_in_map(t_data *l, t_ray *r)
+{
+	if ((int)r->mapy > 0 && (int)r->mapy < l->pars->map_len
+		&& (int)r->mapx > 0
+		&& (size_t)r->mapx < ft_strlen(l->map[(int)r->mapy])
+		&& !ft_isalnum(l->map[(int)r->mapy][(int)r->mapx]))
+		return (1);
+	return (0);
+}
+
+int	ft_not_map(t_data *l, t_ray *r)
+{
+	if (!((int)r->mapy > 0 && (int)r->mapy < l->pars->map_len
+		&& (int)r->mapx > 0
+		&& (size_t)r->mapx < ft_strlen(l->map[(int)r->mapy])))
+		return (1);
+	return (0);
+}
+
 void	ft_dda(t_data *l, t_ray *r)
 {
 	while (!r->hit)
@@ -57,14 +87,12 @@ void	ft_dda(t_data *l, t_ray *r)
 			r->mapy += r->stepy;
 			r->side = 1;
 		}
-		if (l->map[(int)r->mapy] && l->map[(int)r->mapy][(int)r->mapx]
-			&& l->map[(int)r->mapy][(int)r->mapx] != '0')
+		if ((int)r->mapy > 0 && (int)r->mapy < l->pars->map_len
+			&& (int)r->mapx > 0
+			&& (size_t)r->mapx < ft_strlen(l->map[(int)r->mapy])
+			&& ft_not_wall(l, r))
 			r->hit = 1;
-		else if (!ft_isalnum(l->map[(int)r->mapy][(int)r->mapx]))
+		else if (ft_in_map(l, r) || ft_not_map(l, r))
 			break ;
 	}
-	if (r->side == 0)
-		r->perpwalldist = (r->sidedistx - r->deltadistx);
-	else
-		r->perpwalldist = (r->sidedisty - r->deltadisty);
 }
