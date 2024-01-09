371f764f40c1716b6e431c4ef7ba54b578fe2f89
qbnt <quentin.banet@hotmail.fr>
2024-01-09 12:10:52 +0100

    oui

--- srcs/mlx/raycasting/ft_raycasting.c
+++ srcs/mlx/raycasting/ft_raycasting.c
@@ -6,7 +6,7 @@
 /*   By: qbanet <qbanet@student.42perpignan.fr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/21 09:50:36 by mescobar          #+#    #+#             */
-/*   Updated: 2024/01/05 10:57:28 by qbanet           ###   ########.fr       */
+/*   Updated: 2024/01/09 12:05:36 by qbanet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ void	ft_determine_direction(t_data *l)
 	}
 	else if (l->pars->direction == 'E')
 	{
-		l->ray->dirx = -1;
+		l->ray->dirx = 1;
 		l->ray->diry = 0;
 		l->ray->planx = 0;
 		l->ray->plany = -0.66;
